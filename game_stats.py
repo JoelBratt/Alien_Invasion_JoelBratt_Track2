c424f34867a8323f0d8076693f28e69a491fe496
Joel Bratt <jbratt1@student.cscc.edu>
2026-07-23 14:47:24 -0400

    Picking up where tutorial left off as i had some some custom things along the tutorial already

---
+++ game_stats.py
@@ -0,0 +1,68 @@
+from pathlib import Path
+import json
+import math
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from alien_invasion import AlienInvasion
+
+class GameStats():
+
+    def __init__(self, game: 'AlienInvasion'):
+        self.game = game
+        self.settings = game.settings
+        self.max_score = 0
+        self.init_saved_scores()
+        self.reset_stats() 
+
+    def init_saved_scores(self):
+        self.path = self.settings.scores_file
+        if self.path.exists() and self.path.stat.__sizeof__()> 20:
+            contents = self.path.read_text()
+            scores = json.loads(contents)
+            self.hi_score = scores.get('hi_score', 0)
+        else:
+            self.hi_score = 0
+            self.save_score()
+
+    def save_score(self):
+        scores = {
+            'hi_score': self.hi_score
+        }
+        contents = json.dumps(scores, indent=4)
+        try:
+            self.path.write_text(contents)
+        except FileNotFoundError as e:
+            print(f'File Not Found: {e}')
+
+    def reset_stats(self):
+        self.ships_left = self.settings.ships_left
+        self.score = 0
+        self.level = 1
+
+    def update(self, collisions):
+        #update self.score
+        self._update_score(collisions)
+
+        self._update_max_score()
+        self._update_hi_score()
+
+    def _update_max_score(self):
+        if self.score > self.max_score:
+            self.max_score = self.score
+            #print(f'Max: {self.max_score}')
+
+
+    def _update_hi_score(self):
+        if self.score > self.hi_score:
+            self.hi_score = self.score
+            #print(f'hi: {self.hi_score}')
+
+    def _update_score(self, collisions):
+        for alien in collisions.values():
+            self.score += math.ceil(self.settings.alien_points ** (1 + self.level/4))
+            #print(f'Basic: {self.score}')
+
+    def update_level(self):
+        self.level += 1
---
+++ settings.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+class Settings:
+    def __init__(self):
+        self.name: str = 'Alien Invasion'
+        self.screen_w = 1200
+        self.screen_h  = 800
+        self.FPS = 60
+        self.bg_file = Path.cwd() / 'Assets' / 'images' / 'StarBasesnow.png'
+        self.difficulty_scale = 1.15
+        self.scores_file = Path.cwd() / 'Assets' / 'file' / 'scores.json'
+
+        self.ship_file = Path.cwd() / 'Assets' / 'images' / 'ship2(no bg).png'
+        self.ship_w = 40
+        self.ship_h = 60
+
+        self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'laserBlast.png'
+        self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'laser.mp3'
+        self.impact_sound = Path.cwd() / 'Assets' / 'sound' / 'impactSound.mp3'
+
+        self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
+        self.alien_w = 40
+        self.alien_h = 40
+        self.fleet_direction = 1
+
+        self.button_w = 200
+        self.button_h = 50
+        self.button_color = (71, 15, 15)
+
+        self.text_color = (255,255,255)
+        self.button_font_size = (48)
+        self.HUD_font_size = 22
+        self.font_file = Path.cwd() / 'Assets' / 'Fonts' / 'Quantico' / 'Quantico-Bold.ttf'
+
+    def initialize_dynamic_settings(self):
+        self.ship_speed = 10
+        self.ships_left = 3
+
+        self.bullet_w = 25
+        self.bullet_h = 80
+        self.bullet_speed = 15
+        self.bullet_amount = 5
+
+        self.fleet_speed = 2
+        self.fleet_drop_speed = 20
+        self.alien_points = 50
+
+    def increase_difficulty(self):
+        self.ship_speed *= self.difficulty_scale
+        self.bullet_speed *= self.difficulty_scale
+        self.fleet_speed *= self.difficulty_scale
