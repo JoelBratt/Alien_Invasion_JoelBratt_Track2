a33d8aa8e2789c7675bdcf5379152235e0c32858
Joel Bratt <jbratt1@student.cscc.edu>
2026-07-23 17:06:06 -0400

    shrunk bullet to not be seen under ship

--- settings.py
+++ settings.py
@@ -1,23 +1,23 @@
 from pathlib import Path
 class Settings:
     def __init__(self):
-        self.name: str = 'Alien Invasion'
+        self.name: str = '"Man" Invasion - Track 2'
         self.screen_w = 1200
         self.screen_h  = 800
         self.FPS = 60
-        self.bg_file = Path.cwd() / 'Assets' / 'images' / 'StarBasesnow.png'
+        self.bg_file = Path.cwd() / 'Assets' / 'images' / 'BackgroundTest.png'
         self.difficulty_scale = 1.15
         self.scores_file = Path.cwd() / 'Assets' / 'file' / 'scores.json'
 
-        self.ship_file = Path.cwd() / 'Assets' / 'images' / 'ship2(no bg).png'
+        self.ship_file = Path.cwd() / 'Assets' / 'images' / 'ShipReplace.png'
         self.ship_w = 40
         self.ship_h = 60
 
-        self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'laserBlast.png'
+        self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'BlastReplace.png'
         self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'laser.mp3'
         self.impact_sound = Path.cwd() / 'Assets' / 'sound' / 'impactSound.mp3'
 
-        self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
+        self.alien_file = Path.cwd() / 'Assets' / 'images' / 'Alien1.png'
         self.alien_w = 40
         self.alien_h = 40
         self.fleet_direction = 1
@@ -36,7 +36,7 @@ class Settings:
         self.ships_left = 3
 
         self.bullet_w = 25
-        self.bullet_h = 80
+        self.bullet_h = 60
         self.bullet_speed = 15
         self.bullet_amount = 5
 
@@ -48,3 +48,4 @@ class Settings:
         self.ship_speed *= self.difficulty_scale
         self.bullet_speed *= self.difficulty_scale
         self.fleet_speed *= self.difficulty_scale
+        self.bullet_amount += 1
